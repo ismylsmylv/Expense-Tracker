0719f88fda6c2a12c7ae24041b6ca5eb164c9268
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-04-03 21:26:10 +0400

    ui updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./style.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="welcome">Expence Tracker</div>
+      <div className="balance">
+        <div className="heading">your balance</div>
+        <div className="amount">$260.00</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="summary">
+        <div className="incomes summaryCard">
+          <div className="heading">income</div>
+          <div className="amount plus">$500.00</div>
+        </div>
+        <div className="divider"></div>
+        <div className="expenses summaryCard">
+          <div className="heading">expense</div>
+          <div className="amount minus">$240.00</div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="history">
+        <div className="heading">history</div>
+        <div className="line"></div>
+        <div className="list">
+          <div className="card income">
+            <div className="name">cash</div>
+            <div className="price">+500</div>
+          </div>
+          <div className="card expense">
+            <div className="name">book</div>
+            <div className="price">-40</div>
+          </div>
+          <div className="card expense">
+            <div className="name">camera</div>
+            <div className="price">-200</div>
+          </div>
+        </div>
+      </div>
+      <form className="addNew">
+        <div className="heading">add new transaction</div>
+        <div className="line"></div>
+        <div className="nameInput">
+          <div className="head">text</div>
+          <input type="text" placeholder="Enter text..." />
+        </div>
+        <div className="priceInput">
+          <div className="head">amount</div>
+          <div className="explain">(negative - expense, positive - income)</div>
+          <input type="number" placeholder="Enter amount..." />
+        </div>
+        <button>add transaction</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
